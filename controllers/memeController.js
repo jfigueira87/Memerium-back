12180d44b84d31cbd58d6e2c09e485766442adb6
Rada749 <radames5897@gmail.com>
2024-09-25 12:29:57 +0200

    add controller into routes

--- controllers/memeController.js
+++ controllers/memeController.js
@@ -1,19 +1,19 @@
-const getMemes = (req,res) =>{
-    res.send('Get all memes')
-}
+export const getMemes = (req, res) => {
+  res.send("Get all memes");
+};
 
-const getMeme = (req,res) =>{
-    res.send('Get one meme')
-}
+export const getMeme = (req, res) => {
+  res.send("Get one meme");
+};
 
-const createMeme = (req,res) =>{
-    res.send('Create meme')
-}
+export const createMeme = (req, res) => {
+  res.send("Create meme");
+};
 
-const updateMeme = (req,res) =>{
-    res.send('Update meme')
-}
+export const updateMeme = (req, res) => {
+  res.send("Update meme");
+};
 
-const deleteMeme = (req,res) =>{
-    res.send('Delete meme')
-}
+export const deleteMeme = (req, res) => {
+  res.send("Delete meme");
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,27 @@
-import {Router} from 'express'
+import { Router } from "express";
+import {
+  getMemes,
+  getMeme,
+  createMeme,
+  updateMeme,
+  deleteMeme,
+} from "..controllers/memeController.js";
 
-const router = Router()
+const router = Router();
 
 //GET all memes
-router.get('/meme')
+router.get("/meme", getMemes);
 
 //GET one meme by ID
-router.get('/meme/:id')
+router.get("/meme/:id", getMeme);
 
 //CREATE meme
-router.post('/meme')
+router.post("/meme", createMeme);
 
 //PUT meme by ID
-router.put('/meme/:id')
+router.put("/meme/:id", updateMeme);
 
 //DELETE meme
-router.delete('/meme/:id')
+router.delete("/meme/:id", deleteMeme);
 
-export default router
+export default router;
