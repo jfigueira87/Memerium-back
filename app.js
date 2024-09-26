2247b1b9a1d8a146f1d161cd0efab38f37981233
Rada749 <radames5897@gmail.com>
2024-09-26 12:34:42 +0200

    fixed routes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(memeRoutes);
+app.use('/meme', memeRoutes);
 
 console.log("Probando la conexión. CONECTADO");
 
--- routes/routes.js
+++ routes/routes.js
@@ -4,18 +4,18 @@ import { getMeme, getMemes, createMeme, updateMeme, deleteMeme } from "../contro
 const router = Router();
 
 //GET all memes
-router.get("/meme", getMemes);
+router.get("/", getMemes);
 
 //GET one meme by ID
-router.get("/meme/:id", getMeme);
+router.get("/:id", getMeme);
 
 //CREATE meme
-router.post("/meme", createMeme);
+router.post("/", createMeme);
 
 //PUT meme by ID
-router.put("/meme/:id", updateMeme);
+router.put("/:id", updateMeme);
 
 //DELETE meme
-router.delete("/meme/:id", deleteMeme);
+router.delete("/:id", deleteMeme);
 
 export default router;
