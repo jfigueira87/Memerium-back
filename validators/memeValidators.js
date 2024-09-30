67edf8a5373eb965a704a455bff6aad9ec366c20
jfigueira87 <jfigueira@gmail.com>
2024-09-30 12:52:45 +0200

    Update validator

--- validators/memeValidators.js
+++ validators/memeValidators.js
@@ -1,10 +1,5 @@
 import { check, validationResult } from 'express-validator';
-
-export const validateMemeId = [
-    check('id')
-      .isInt()
-      .withMessage('El ID debe ser un número entero'),
-  ];
+import { body, param } from 'express-validator';
 
 export const handleValidation = (req, res, next) => {
     const errors = validationResult(req);
@@ -13,3 +8,33 @@ export const handleValidation = (req, res, next) => {
     }
     next();
   };
+
+  export const validateCreateOrUpdate = [
+    body('title')
+      .notEmpty()
+      .withMessage('🚨El título es obligatorio🚨')
+      .isString()
+      .withMessage('🚨El título debe ser un texto🚨'),
+    body('category')
+      .notEmpty()
+      .withMessage('🚨La categoría es obligatoria🚨')
+      .isString()
+      .withMessage('🚨La categoría debe ser un texto🚨'),
+    body('tags')
+      .notEmpty()
+      .withMessage('🚨Los tags son obligatorios🚨')
+      .isString()
+      .withMessage('🚨Los tags deben ser un texto🚨'),
+    body('url')
+      .notEmpty()
+      .withMessage('🚨La URL es obligatoria🚨')
+      .isURL()
+      .withMessage('🚨Debe proporcionar una URL válida🚨')
+  ];
+  
+  // Validaciones para rutas que incluyen el ID como parámetro
+  export const validateIdParam = [
+    param('id')
+      .isInt()
+      .withMessage('🚨El ID debe ser un número entero🚨')
+  ];
