009ec629d43a68f8323af6c7b984ffc0239808d6
Rada749 <radames5897@gmail.com>
2024-10-01 14:24:47 +0200

    fixed db env

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 import { DB_KEY, DB_NAME, DB_USER } from "../config.js";
 
-const conection_db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_KEY, {
+const conection_db = new Sequelize(DB_NAME, DB_USER, DB_KEY, {
   host: "localhost",
   dialect: "mysql",
   define: {
